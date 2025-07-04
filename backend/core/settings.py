d0c7f58c1fa4a692243740d165dc726ce02d67b6
md-hafizur <mdhafizur.cse@gmail.com>
2025-07-04 20:22:42 +0600

    solve migrations

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.accounts',
     'apps.address',
     'apps.payment',
 ]
